0e982d8e26aa556030b87b4a9e98787bad9c4cef
ykmxxi <ykmxxi97@gmail.com>
2023-01-14 17:17:20 +0900

    feat: model 구현

---
+++ src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,31 @@
+package org.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "todoOrder", nullable = false)
+    private Long order;
+
+    @Column(nullable = false)
+    private Boolean completed;
+}
