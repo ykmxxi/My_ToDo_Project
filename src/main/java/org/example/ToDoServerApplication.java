309b634f5c3ce9049742913bdff13e4279c3ddd4
ykmxxi <ykmxxi97@gmail.com>
2023-01-14 18:07:03 +0900

    feat(TodoController): controller 구현

--- src/main/java/org/example/ToDoServerApplication.java
+++ src/main/java/org/example/ToDoServerApplication.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ToDoServerApplication {
     public static void main(String[] args) {
-        // Project Setting
+        SpringApplication.run(ToDoServerApplication.class, args);
     }
 }
